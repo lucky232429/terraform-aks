1330e9e2672b24479b4772827ce3b6aa492ff1aa
lucky232429 <127707678+lucky232429@users.noreply.github.com>
2023-03-12 23:22:17 -0400

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,21 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>2.95.0"
+      version = "~>3.47.0"
     }
   }
   backend "azurerm" {
-    // storage_account_name        = "codechallengechn"
-    // resource_group_name         = "Terraform-State-RG"
-    // container_name              = "tfstate"
-    // key                         = "terraform.tfstate"
-    // access_key                  = "6o8jCbOtThyyC9IduWSnwtCblEZkrzKZTzXIwNWl5cvs1t2QI896cUtuJU/Ae6sQN7Nh+PtpEiij+AStGrypJQ=="
-
     storage_account_name        = ""
     resource_group_name         = ""
     container_name              = ""
     key                         = ""
-    // access_key                  = ""
   }
 }
 
